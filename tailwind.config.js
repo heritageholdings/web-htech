c857d26f7be0b72decf3caae8e9763194fbe399d
Federico Feroldi <federico@heritageholdings.co>
2022-01-13 13:19:04 +0100

    Basic Tailwind integration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  purge: [
-    "./_includes/**/*.html",
-    "./_layouts/**/*.html",
-    "./_posts/*.md",
-    "./*.html",
-  ],
+  purge: ["./_includes/**/*.html", "./_layouts/**/*.html", "./*.html"],
   darkMode: false,
   theme: {
     extend: {},
