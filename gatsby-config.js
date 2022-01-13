1b09871f13260103163b41d77c9efb3923a3c813
Federico Feroldi <federico@heritageholdings.co>
2022-01-13 19:02:22 +0100

    Basic hero landing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
 module.exports = {
+  pathPrefix: `/web-htech`, // FIXME: remove once we deploy to domain
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://heritageholdings.github.io/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
