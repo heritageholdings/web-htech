d13c45dfdb99e008d929fc3d7299c20886a76aac
Federico Feroldi <federico@heritageholdings.co>
2022-02-03 11:18:06 +0100

    Make emails actual links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,8 @@ export const Footer: React.FC<{}> = () => (
     <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8 border-t border-gray-200 mt-12">
       <div className="mt-8 md:mt-0 md:order-1">
         <p className="text-center text-base text-gray-400">
-          &copy; 2022 HTECH S.r.l. &bull; VAT IT11713660964
+          &copy; 2022 HTECH S.r.l. &bull; VAT IT11713660964 &bull; Corso di
+          Porta Nuova, 34 - 20121, Milano
         </p>
       </div>
     </div>
--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -16,46 +16,16 @@ const ContactsPage = () => (
             <div className="mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 lg:mt-0 lg:col-span-2">
               <div>
                 <h3 className="text-lg leading-6 font-medium text-gray-900">
-                  Collaborate
-                </h3>
-                <dl className="mt-2 text-base text-gray-500">
-                  <div>
-                    <dt className="sr-only">Email</dt>
-                    <dd>support@example.com</dd>
-                  </div>
-                  <div className="mt-1">
-                    <dt className="sr-only">Phone number</dt>
-                    <dd>+1 (555) 123-4567</dd>
-                  </div>
-                </dl>
-              </div>
-              <div>
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
-                  Press
-                </h3>
-                <dl className="mt-2 text-base text-gray-500">
-                  <div>
-                    <dt className="sr-only">Email</dt>
-                    <dd>support@example.com</dd>
-                  </div>
-                  <div className="mt-1">
-                    <dt className="sr-only">Phone number</dt>
-                    <dd>+1 (555) 123-4567</dd>
-                  </div>
-                </dl>
-              </div>
-              <div>
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
                   Join our team
                 </h3>
                 <dl className="mt-2 text-base text-gray-500">
                   <div>
                     <dt className="sr-only">Email</dt>
-                    <dd>support@example.com</dd>
-                  </div>
-                  <div className="mt-1">
-                    <dt className="sr-only">Phone number</dt>
-                    <dd>+1 (555) 123-4567</dd>
+                    <dd>
+                      <a href="mailto:careers@teamhtech.com">
+                        careers@teamhtech.com
+                      </a>
+                    </dd>
                   </div>
                 </dl>
               </div>
@@ -66,11 +36,9 @@ const ContactsPage = () => (
                 <dl className="mt-2 text-base text-gray-500">
                   <div>
                     <dt className="sr-only">Email</dt>
-                    <dd>support@example.com</dd>
-                  </div>
-                  <div className="mt-1">
-                    <dt className="sr-only">Phone number</dt>
-                    <dd>+1 (555) 123-4567</dd>
+                    <dd>
+                      <a href="mailto:info@teamhtech.com">info@teamhtech.com</a>
+                    </dd>
                   </div>
                 </dl>
               </div>
@@ -86,8 +54,8 @@ const ContactsPage = () => (
                   Italy
                 </h3>
                 <div className="mt-2 text-base text-gray-500">
-                  <p>Corso di Porta Nuova, 34</p>
-                  <p className="mt-1">20121, Milano</p>
+                  <p>Piazza San Babila, 5</p>
+                  <p className="mt-1">20122, Milano</p>
                 </div>
               </div>
             </div>
